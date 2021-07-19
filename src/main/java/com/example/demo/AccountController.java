95f36492e74ec803c0e6ba2809f6723dd3768609
id <id@IN0013391>
2021-07-19 15:27:56 +0900

    7/19(月)pm作業分

--- src/main/java/com/example/demo/AccountController.java
+++ src/main/java/com/example/demo/AccountController.java
@@ -190,8 +190,8 @@ public class AccountController {
 		date = date.length() == 1 ? "0" + date : date;
 		String scheduledate = year + "/" + month + "/" + date;
 		String scheduledates = year + "/" + month;
-		String times[] = { "1:00", "1:30", "2:00", "2:30", "3:00", "3:30", "4:00", "4:30", "5:00", "5:30", "6:00",
-				"6:30", "7:00", "7:30", "8:00", "8:30", "9:00", "9:30", "10:00", "10:30", "11:00", "11:30", "12:00",
+		String times[] = { "01:00", "01:30", "02:00", "02:30", "03:00", "03:30", "04:00", "04:30", "05:00", "05:30", "06:00",
+				"06:30", "07:00", "07:30", "08:00", "08:30", "09:00", "09:30", "10:00", "10:30", "11:00", "11:30", "12:00",
 				"12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30",
 				"18:00", "18:30", "19:00", "19:30", "20:00", "20:30", "21:00", "21:30", "22:00", "22:30", "23:00",
 				"23:30", "24:00", "24:30" };
@@ -270,7 +270,8 @@ public class AccountController {
 			@RequestParam("schedule") String schedule,
 			@RequestParam("schedulememo") String schedulememo,
 			ModelAndView mv) {
-
+		starthour = starthour.length() == 1 ? "0" + starthour : starthour;
+		endhour = endhour.length() == 1 ? "0" + endhour : endhour;
 		Account account = (Account) session.getAttribute("customerInfo");
 		MyDate hizuke = (MyDate) session.getAttribute("sche");
 		int category_code = account.getCode();
